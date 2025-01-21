c335e437248f9a0050d5725221c8c5fb8537d4d0
Vasiliy Dommes <vasdommes@gmail.com>
2025-01-21 16:34:48 -0500

    Add unit tests for parsing and evaluating Linear_Combination_Of_Mathematica_Functions

--- test/src/unit_tests/cases/simpleboot.test.cxx
+++ test/src/unit_tests/cases/simpleboot.test.cxx
@@ -1,16 +1,17 @@
-#include <catch2/catch_amalgamated.hpp>
-#include "pmp_read/simpleboot/data_provider/Abstract_Simpleboot_Data_Provider_Evaluated.hxx"
 #include "pmp_read/simpleboot/Mathematica_Simpleboot_Expression_Parser.hxx"
+#include "pmp_read/simpleboot/data_provider/Abstract_Simpleboot_Data_Provider.hxx"
 #include "unit_tests/util/util.hxx"
+
+#include <catch2/catch_amalgamated.hpp>
 
 using Test_Util::REQUIRE_Equal::diff;
 namespace
 {
   struct Simpleboot_Test_Data_Provider final
-      : Abstract_Simpleboot_Data_Provider_Evaluated
+      : Abstract_Simpleboot_Data_Provider
   {
     explicit Simpleboot_Test_Data_Provider(const Simpleboot_Parameters &params)
-        : Abstract_Simpleboot_Data_Provider_Evaluated(params)
+        : Abstract_Simpleboot_Data_Provider(params)
     {}
 
     // Set default simpleboot parameters to arbitrary numbers
@@ -27,10 +28,15 @@ namespace
                                   {}})
     {}
 
+    [[nodiscard]] int get_index(const std::string &, const int L) const
+    {
+      return L;
+    }
+
   protected:
     // Generate some arbitrary polynomial
-    Polynomial blockF_lookup(const std::string &stamp, const int L,
-                             const int m, const int n) override
+    Polynomial blockF_lookup([[maybe_unused]] const std::string &stamp,
+                             const int L, const int m, const int n) override
     {
       Polynomial poly(L + 5, 0);
       for(auto i = 0; i < poly.coefficients.size(); i++)
@@ -74,5 +80,36 @@ TEST_CASE("simpleboot")
         CAPTURE(result);
       }
     }
+  }
+
+  SECTION("Linear_Combination_Of_Mathematica_Functions")
+  {
+    INFO("Parse Linear_Combination_Of_Mathematica_Functions and evaluate it in parallel.");
+    const auto provider = std::make_shared<Simpleboot_Test_Data_Provider>();
+    Mathematica_Simpleboot_Expression_Parser<Simpleboot_Test_Data_Provider,
+                                             /*evaluate_F0_F_FS=*/false>
+      parser(provider);
+
+    const std::string input = GENERATE( "FS[\"stamp\",1,2,3,-4.5*^-7]",
+      "1.3 * FS[\"stamp\",1,2,3,-4.5*^-1] - F[\"stamp\",2,2,3,0.45] - 1.7*^-2 * F0[0.34,1,2]"
+      );
+    CAPTURE(input);
+    const auto begin = input.c_str();
+    const auto end = begin + input.size();
+
+    MMA_ELEMENT result;
+    parser.parse_element(begin, end, result);
+    auto funcs = AS_MMA_ELEMENT_Linear_Combination_Of_Functions(result);
+
+    INFO("NB: we only check that parallel and non-parallel computations give the same result.");
+    INFO("TODO: check that this result is actually correct.");
+
+    // compute everything on a single rank
+    auto result_self
+      = evaluate_parallel({funcs}, *provider, El::mpi::COMM_SELF);
+    // compute different functions on different ranks
+    auto result_world
+      = evaluate_parallel({funcs}, *provider, El::mpi::COMM_WORLD);
+    DIFF(result_self, result_world);
   }
 }
